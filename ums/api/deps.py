708202d2626e80ea191f6e46a0256cca86192645
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-06-20 21:24:32 +0000

    feat: Add dependencies for user management API endpoints

---
+++ ums/api/deps.py
@@ -0,0 +1,11 @@
+from fastapi import Path, Query
+from typing import Annotated
+from fastapi import Depends
+from ums.database import get_db
+from sqlalchemy.orm import Session
+
+
+DBSessionDependency = Annotated[Session, Depends(get_db)]
+
+UsernameDependency = Query(None, description="The username of the user")
+UserIdDependency = Path(..., description="The ID of the user")
